eb153a55bc1507a1ce9509fec45a724eccc8cb2c
OmidGholi <gholio@yahoo.com>
2022-12-11 22:10:34 -0800

    3

--- main.py
+++ main.py
@@ -2,42 +2,52 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-cols = ["venue", "date", "time", "status", "count"]
-df = pd.read_csv(r"/Users/omidgholizadeh/Documents/occupancy_data_new.csv",names=cols).drop_duplicates()
 
-df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True, cache=True, errors="coerce")
-df['time'] = pd.to_datetime(df['time'], infer_datetime_format=True, cache=True, errors="coerce").dt.hour
+#Make sure dates are in correct format
+def gym_analysis(venue, start_date, end_date):
+
+    cols = ["venue", "date", "time", "status", "count"]
+    df = pd.read_csv(r"/Users/omidgholizadeh/Documents/occupancy_data_new.csv",names=cols).drop_duplicates()
+
+    df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True, cache=True, errors="coerce")
+    df['time'] = pd.to_datetime(df['time'], infer_datetime_format=True, cache=True, errors="coerce").dt.hour
+
+    start_time = 0
+    end_time = 24
 
 
-venue = "ALL"
-start_date = "2020-01-01"
-end_date = "2023-01-31"
-start_time = 0
-end_time = 24
-
-try:
-    if venue == "ALL":
-        data = df[df["status"].eq("Open") & df["date"].between(start_date, end_date) & df['time'].between(start_time, end_time)]
+    if start_date is None and end_date is None:
+        if venue == "ALL":
+            data = df[
+                df["status"].eq("Open") & df['time'].between(start_time,end_time)]
+        else:
+            data = df[df["venue"].eq(venue) & df["status"].eq("Open") & df['time'].between(start_time, end_time)]
     else:
-        data = df[df["venue"].eq(venue) & df["status"].eq("Open") & df["date"].between(start_date, end_date) & df['time'].between(start_time, end_time)]
-except:
-    print("Error, Please try a different date range")
-    exit()
+        try:
+            if venue == "ALL":
+                data = df[df["status"].eq("Open") & df["date"].between(start_date, end_date) & df['time'].between(start_time,end_time)]
+            else:
+                data = df[df["venue"].eq(venue) & df["status"].eq("Open") & df["date"].between(start_date, end_date) & df['time'].between(start_time, end_time)]
+        except:
+            print("Error, Please Select a different date range")
+            exit()
+
+    bottom_quartile = round(data["count"].quantile(0.25))
+    top_quartile = round(data["count"].quantile(0.75))
+    average = round(data["count"].mean())
+    print(f"Bottom Quartile: {bottom_quartile}\nAverage: {average}\nTop Quartile: {top_quartile}")
+    bottom_quart = data[data["count"].between(bottom_quartile, top_quartile)]
+    # print(bottom_quart)
 
 
-bottom_quartile = round(data["count"].quantile(0.25))
-top_quartile = round(data["count"].quantile(0.75))
-average = round(data["count"].mean())
-print(f"Bottom Quartile: {bottom_quartile}\nAverage: {average}\nTop Quartile: {top_quartile}")
-bottom_quart = data[data["count"].between(bottom_quartile, top_quartile)]
-# print(bottom_quart)
 
 
-
-
-ax = sns.lineplot(data=data, x="time", y="count", hue="venue", ci=None)
-ax.set(xlabel="Time", ylabel="Count")
-ax.set_title(f"{venue} venues from {start_date} to {end_date} and {start_time}:00 to {end_time}:00")
-plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
-plt.show()
-plt.savefig("test.png")
+    ax = sns.lineplot(data=data, x="time", y="count", hue="venue", ci=None)
+    ax.set(xlabel="Time", ylabel="Count")
+    if start_date is None and end_date is None:
+        ax.set_title(f"Occupancy Data for {venue}")
+    else:
+        ax.set_title(f"Occupancy Data for {venue} from {start_date} to {end_date}")
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
+    plt.show()
+    plt.savefig("test.png")
