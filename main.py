4c3c89529baa49903eea53f656bdb3cab2e8eb53
OmidGholi <gholi.o@yahoo.com>
2022-12-12 18:25:30 -0800

    Added Age Verification

--- main.py
+++ main.py
@@ -9,106 +9,103 @@ import statistics
 import pandas as pd
 import datetime
 
-
-def serve_end_cal():
-    """This is a function that"""
-    global end_cal
-    global end_button
-    start_button.config(state="disabled")
-    end_cal = DateEntry(root, mindate=start_cal.get_date(), maxdate=date.today())
-    end_cal.grid(pady=5)
-    end_button = tk.Button(root, text="Submit End Range", command=get_date, font=6)
-    end_button.grid(pady=5)
-
-
-def get_date():
-    """This is a function that"""
-    select_venue()
-    end_button.config(state="disabled")
+def main():
+    def serve_end_cal():
+        """This is a function that"""
+        global end_cal
+        global end_button
+        start_button.config(state="disabled")
+        end_cal = DateEntry(root, mindate=start_cal.get_date(), maxdate=date.today())
+        end_cal.grid(pady=5)
+        end_button = tk.Button(root, text="Submit End Range", command=get_date, font=6)
+        end_button.grid(pady=5)
 
 
-def range_data():
-    """This is a function that"""
-    all_button.destroy()
-    range_button.destroy()
-    start_cal.grid(pady=5)
-    start_button.grid(pady=5)
+    def get_date():
+        """This is a function that"""
+        select_venue()
+        end_button.config(state="disabled")
 
 
-def all_data():
-    """This is a function that"""
-    all_button.destroy()
-    range_button.destroy()
-    select_venue()
+    def range_data():
+        """This is a function that"""
+        all_button.destroy()
+        range_button.destroy()
+        start_cal.grid(pady=5)
+        start_button.grid(pady=5)
 
 
-def analysis():
-    """This is a function that"""
-    try:
-        start_date = pd.to_datetime(start_cal.get_date(), infer_datetime_format=True, cache=True)
-        end_date = pd.to_datetime(end_cal.get_date(), infer_datetime_format=True, cache=True)
-    except:
-        start_date = None
-        end_date = None
+    def all_data():
+        """This is a function that"""
+        all_button.destroy()
+        range_button.destroy()
+        select_venue()
 
 
-    print(start_date, end_date)
-    venue = clicked.get()
-    print(venue)
-    root.destroy()
-    statistics.gym_analysis(venue, start_date, end_date)
-    tk.messagebox.showinfo(message="Report Successfully Saved to Data.csv")
+    def analysis():
+        """This is a function that"""
+        try:
+            start_date = pd.to_datetime(start_cal.get_date(), infer_datetime_format=True, cache=True)
+            end_date = pd.to_datetime(end_cal.get_date(), infer_datetime_format=True, cache=True)
+        except:
+            start_date = None
+            end_date = None
 
 
-def select_venue():
-    """This is a function that"""
-    range_selection_label.destroy()
-    venue_label = tk.Label(text="Please select a venue you would like to analyse from the options below:", bg="red2", fg="black", font=8)
-    venue_label.grid(pady=5)
-    global clicked
-    clicked = StringVar(root)
-    clicked.set("ALL")
-    venue_options = ["ALL","Entry","Arc Floor One", "Climbing", "Arc Olympic Lifting Zones", "Arc Floor Two",
-                                     "South Court", "Four Court Gym", "North Court", "Recreation Pool", "Competition Pool",
-                                     "Arc Express", "Spa", "Aquaplex Pool Deck", "Tennis Courts"]
-    venue_select = OptionMenu(root, clicked, *venue_options)
-    venue_select.grid(pady=5)
-
-    analysis_button = tk.Button(root, text="Submit", command=analysis, font=6)
-    analysis_button.grid(pady=5)
-    analysis_label= tk.Label(root,text="*Clicking submit will run the analysis*", bg="red2", fg="black", font=6)
-    analysis_label.grid(pady=5)
+        print(start_date, end_date)
+        venue = clicked.get()
+        print(venue)
+        root.destroy()
+        statistics.gym_analysis(venue, start_date, end_date)
+        tk.messagebox.showinfo(message="Report Successfully Saved to Data.csv")
 
 
-root = tk.Tk()
-root.title("ARC Gym Analysis")
-root.configure(bg="red2")
-greeting_label = tk.Label(text="Hello and Welcome to the...", bg="red2", fg="black", font=12)
-greeting_label.grid(pady=5)
-image = ImageTk.PhotoImage(Image.open("ARC_Image.png"))
-serve_image = Label(root, image=image)
-serve_image.grid()
-date_label = tk.Label(text="How would you like to sort through the data?", bg="red2", fg="black")
-# range and all buttons
-gym_analyzer_label = tk.Label(text="ARC Gym Analyzer!", bg="red2", fg="black", font=12)
-gym_analyzer_label.grid(pady=5)
+    def select_venue():
+        """This is a function that"""
+        range_selection_label.destroy()
+        venue_label = tk.Label(text="Please select a venue you would like to analyse from the options below:", bg="red2", fg="black", font=8)
+        venue_label.grid(pady=5)
+        global clicked
+        clicked = StringVar(root)
+        clicked.set("ALL")
+        venue_options = ["ALL","Entry","Arc Floor One", "Climbing", "Arc Olympic Lifting Zones", "Arc Floor Two",
+                                         "South Court", "Four Court Gym", "North Court", "Recreation Pool", "Competition Pool",
+                                         "Arc Express", "Spa", "Aquaplex Pool Deck", "Tennis Courts"]
+        venue_select = OptionMenu(root, clicked, *venue_options)
+        venue_select.grid(pady=5)
 
-range_selection_label = tk.Label(text="Please select either a range of dates or all dates for running analysis:", bg="red2", fg="black", font=8)
-range_selection_label.grid(pady=5)
-
-all_button = tk.Button(text="All Dates", command=all_data, bg="white", fg="black", font=6)
-range_button = tk.Button(text="Date Range", command=range_data, bg="white", fg="black", font=6)
-# position on range buttons
-all_button.grid(pady=5)
-range_button.grid(pady=5)
+        analysis_button = tk.Button(root, text="Submit", command=analysis, font=6)
+        analysis_button.grid(pady=5)
+        analysis_label= tk.Label(root,text="*Clicking submit will run the analysis*", bg="red2", fg="black", font=6)
+        analysis_label.grid(pady=5)
 
 
+    root = tk.Tk()
+    root.title("ARC Gym Analysis")
+    root.configure(bg="red2")
+    greeting_label = tk.Label(text="Hello and Welcome to the...", bg="red2", fg="black", font=12)
+    greeting_label.grid(pady=5)
+    image = ImageTk.PhotoImage(Image.open("ARC_Image.png"))
+    serve_image = Label(root, image=image)
+    serve_image.grid()
+    date_label = tk.Label(text="How would you like to sort through the data?", bg="red2", fg="black")
+    # range and all buttons
+    gym_analyzer_label = tk.Label(text="ARC Gym Analyzer!", bg="red2", fg="black", font=12)
+    gym_analyzer_label.grid(pady=5)
 
-start_cal = DateEntry(root, maxdate=date.today())
+    range_selection_label = tk.Label(text="Please select either a range of dates or all dates for running analysis:", bg="red2", fg="black", font=8)
+    range_selection_label.grid(pady=5)
 
-start_button = tk.Button(text="Submit Start Range", command=serve_end_cal, font=6)
+    all_button = tk.Button(text="All Dates", command=all_data, bg="white", fg="black", font=6)
+    range_button = tk.Button(text="Date Range", command=range_data, bg="white", fg="black", font=6)
+    # position on range buttons
+    all_button.grid(pady=5)
+    range_button.grid(pady=5)
 
+    
 
+    start_cal = DateEntry(root, maxdate=date.today())
 
-if __name__ == '__main__':
+    start_button = tk.Button(text="Submit Start Range", command=serve_end_cal, font=6)
+
     root.mainloop()
